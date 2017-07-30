11ab997d8f3fde24acfa9bd27fd40bbb45182c49
l-emi <lemi.dev@gmail.com>
2017-07-30 21:07:20 +0100

    finish app

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-var express = require('express');
-// var router = express.Router();
-var cors = require('cors');
-var app = express();
-var port = process.env.PORT || 8080;
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 8080;
 
 app.use(cors());
-// app.use(router);
 
-app.get('/', function(req, res) {
-    
+app.get('/', (req, res) => {
+    res.send('go to api/whoami');
 });
+
+app.get('/api/whoami', (req, res) => {
+    let info = {
+        'ipaddress': req.ip,
+        'language': req.headers['accept-language'].split(',')[0],
+        'software': req.headers["user-agent"].split("(")[1].split(")")[0]
+    };
+
+    console.log(JSON.stringify(info));
+    res.set('Content-Type', 'application/json');
+    res.json(info);
+});
+
+app.listen(port, () => {
+    console.log('Listening on port: ', port);
+}); 
